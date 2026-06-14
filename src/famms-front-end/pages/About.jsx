77aced01d7eb37e336d57f4c330889faf60b86b5
shruthi06071995 <shruthiganesan518@gmail.com>
2026-06-14 21:32:51 +0530

    edits

--- src/famms-front-end/pages/About.jsx
+++ src/famms-front-end/pages/About.jsx
@@ -1,8 +1,10 @@
 import { Container, Card, Col, Row } from "react-bootstrap";
-import '../pages//pages.css'
+import '../pages/pages.css'
+import '../home/Home.css'
 import truck from '../../assets/truck.png'
 import free from '../../assets/free.png'
 import award from '../../assets/reward.png'
+import arrival from '../../assets/arrival.png'
 
 function About() {
     return (
@@ -12,14 +14,15 @@ function About() {
                 <Container>
                     <h2 className="section-title"><b>Why Shop With Us</b></h2>
                     <div className="underline"></div>
-                    <Row className="about-row g-4">
+                    <Row className="about-row">
                         <Col xl={4} lg={4} md={6} sm={12}>
                             <Card className="h-100" >
                                 <Card.Body className="about-card" >
                                     <img src={truck} alt="icon tuck" className="icon-img" />
                                     <Card.Title><b>Fast Delivery</b></Card.Title>
                                     <Card.Text>
-                                        Variation of passages of Lorem <br /> Ipsum avaliable
+                                        variations of passages of Lorem
+                                        Ipsum available
                                     </Card.Text>
                                 </Card.Body>
                             </Card>
@@ -30,7 +33,8 @@ function About() {
                                     <img src={free} alt="icon tuck" className="icon-img" />
                                     <Card.Title><b>Free Shipping</b></Card.Title>
                                     <Card.Text>
-                                        Variation of passages of Lorem <br />Ipsum avaliable
+                                        variations of passages of Lorem
+                                        Ipsum available
                                     </Card.Text>
                                 </Card.Body>
                             </Card>
@@ -41,7 +45,8 @@ function About() {
                                     <img src={award} alt="icon tuck" className="icon-img" />
                                     <Card.Title><b>Best Quality</b></Card.Title>
                                     <Card.Text>
-                                        Variation of passages of Lorem <br /> Ipsum avaliable
+                                        variations of passages of Lorem
+                                        Ipsum available
                                     </Card.Text>
                                 </Card.Body>
                             </Card>
@@ -49,6 +54,37 @@ function About() {
                     </Row>
                 </Container>
             </section>
+
+            {/* Arrival Section  */}
+            <section className="arrival-section">
+                <Container>
+                    <Row className="align-items-center arrival-row">
+
+                        <Col md={7} className="arrival-img">
+                            <img
+                                src={arrival}
+                                alt="arrival"
+                                className="img-fluid"
+                            />
+                        </Col>
+
+                        <Col md={5} className="arrival-content">
+                            <h1>#NewArrivals</h1>
+
+                            <p>
+                                Vitae fugiat laboriosam officia perferendis provident
+                                aliquid voluptatibus dolorem...
+                            </p>
+
+                            <button className="shop-btn">
+                                Shop Now
+                            </button>
+                        </Col>
+
+                    </Row>
+                </Container>
+            </section>
+            {/* Arrival Section ends  */}
         </>
     );
 }
